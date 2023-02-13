b5487b043ebe0a47ea96480a54b63b3423830522
Mahmoud Elshafaey <mahmoud.elshafaey@linkdev.com>
2023-02-13 15:09:58 +0200

    docker image

---
+++ wordpress/salt.php
@@ -0,0 +1,9 @@
+<?php 
+define('AUTH_KEY',         '_n wG_J$+^;erpvjH<j~wS}(Nd?h4`|/QBT!8H=22F)-!8DQqc]qGq= w3XBf^0b');
+define('SECURE_AUTH_KEY',  'PF*W^SRkDC?*hiYwfl+U7gfy-G9r:_axVM[Ui=2k[z@m7>$ARdDr-u)y)Y?.lZ$+');
+define('LOGGED_IN_KEY',    'i7#hn-}]DO}9o)L(%C0)*c]e>.H:[%F]rnV!4eHTM-+w<Vmq/:frD8K(uhT;`]2@');
+define('NONCE_KEY',        'Ez8z<SBmC~|1y#doxDVZ1.6i+l%n,*.=AZvSl%w6e+L1P{#lr?*z+b!%tvMGB-rn');
+define('AUTH_SALT',        '(wG>:$zY81ABa-n+,f<2eR0T#Yj*;[>]MNZ3_]1rDI$:7aFlhrt-|N?9-??;yaKX');
+define('SECURE_AUTH_SALT', 'n=<$!S(& v&Xu[2<Pf%HoxCX]hKmk|U3EwV:|cjQ9R{!X&Ap3OgZcxGwAPtyX9rb');
+define('LOGGED_IN_SALT',   '{12f%$|aB}wW<t-C|Aa1^li<ev Q[L*vz--$=x`F_q #-mM-wv-<F-/y}(ccHc(p');
+define('NONCE_SALT',       'od|x41F3x7/~|R8L|o3B:R&q8]zBO3D&q.M*eBb|2(3r*4dQWkC(lv-|+rI^- pB');
